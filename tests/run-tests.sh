36d6b53e2a3cdaba6b08dbf097db5de4e0f53af8
Nate Marti <nate_marti@sil.org>
2021-09-01 17:34:43 -0500

    turn off verbose output

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,5 +3,10 @@ file_path=$(readlink -f "$0")
 root_dir=$(dirname $(dirname "$file_path"))
 cd "${root_dir}"
 
-python3 -m unittest discover -v -s tests/unit
-python3 -m unittest discover -v -s tests/integration
+# Verbose output.
+# python3 -m unittest discover -v -s tests/unit
+# python3 -m unittest discover -v -s tests/integration
+
+# Simple output.
+python3 -m unittest discover -s tests/unit
+python3 -m unittest discover -s tests/integration
