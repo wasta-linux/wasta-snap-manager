9b06994eaac4e8233a810723f5bbe05ca0cf65f7
Nate Marti <nate_marti@sil.org>
2024-09-26 15:53:57 +0100

    get module version from python module, not debian/changelog

--- setup.py
+++ setup.py
@@ -6,32 +6,38 @@ from distutils.core import setup
 from pathlib import Path
 
 
+def get_version():
+    version = '0.0.0'
+    wsm_init = Path(__file__).parent / 'wsm' / '__init__.py'
+    with wsm_init.open() as f:
+        for line in f:
+            if line.startswith('__version__'):
+                version = line.split('=')[1].strip().strip("'")
+    return version
+
+
 def get_vars():
     vars = {}
 
+    # Get version number.
+    vars['version'] = get_version()
+
     # Get details from debian/* files.
     debian_dir = Path(__file__).parents[0] / 'debian'
-    # Get version number from debian/changelog.
-    changelog = debian_dir / 'changelog'
-    with open(changelog) as f:
-        first_line = f.readline()
-    # 2nd term in 1st line; need to remove parentheses.
-    vars['version'] = first_line.split()[1][1:-1]
-
     # Get contents of debian/control.
     control = debian_dir / 'control'
     with open(control) as f:
         text = f.read()
     # Get author and email.
-    m = re.search('Maintainer:\s(.*)\n', text)
+    m = re.search(r'Maintainer:\s(.*)\n', text)
     contact = m.group(1).split('<')
     vars['author'] = contact[0].strip()
     vars['email'] = contact[1].strip().rstrip('>')
     # Get url.
-    u = re.search('Homepage:\s(.*)\n', text)
+    u = re.search(r'Homepage:\s(.*)\n', text)
     vars['url'] = u.group(1)
     # Get description.
-    d = re.search('Description:\s(.*)\n', text)
+    d = re.search(r'Description:\s(.*)\n', text)
     vars['description'] = d.group(1)
     return vars
 
@@ -39,7 +45,7 @@ def get_vars():
 vars = get_vars()
 
 
-if __name__ == '__main__': # pragma: no cover
+if __name__ == '__main__':  # pragma: no cover
     setup(
         name='Wasta [Snap] Manager',
         version=vars.get('version'),
@@ -52,7 +58,7 @@ if __name__ == '__main__': # pragma: no cover
         scripts=['scripts/wasta-snap-manager'],
         data_files=[
             ('share/polkit-1/actions', glob.glob('data/actions/*')),
-            ('share/icons/hicolor/scalable/apps', glob.glob('data/icons/*.svg')),
+            ('share/icons/hicolor/scalable/apps', glob.glob('data/icons/*.svg')),  # noqa: E501
             ('share/wasta-snap-manager/ui', glob.glob('data/ui/*.glade')),
             ('share/applications', glob.glob('data/applications/*.desktop')),
         ]
